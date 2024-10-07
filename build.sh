04cf66056cd47f016c3a9f9abb78cc2878cf914b
Katharine Chui <katharine.chui@gmail.com>
2024-10-07 13:34:03 +0200

    specify -O0 during building instead of just assuming

--- build.sh
+++ build.sh
@@ -17,10 +17,10 @@ do
 	CPPC=${arch}-w64-mingw32-g++
 	CC=${arch}-w64-mingw32-gcc
 	$CC -g -fPIC -c logging.c -o $OUT_DIR/logging.o
-	$CPPC -g -fPIC -c common.cpp -std=c++20 -o $OUT_DIR/common.o
-	$CPPC -g -fPIC -c config.cpp -Ijson_hpp -std=c++20 -o $OUT_DIR/config.o
-	$CPPC -g -fPIC -c dinput8_hook_adapter.cpp -std=c++20 -o $OUT_DIR/dinput8_hook_adapter.o
-	$CPPC -g -fPIC -c process.cpp -std=c++20 -o $OUT_DIR/process.o
+	$CPPC -g -fPIC -c common.cpp -std=c++20 -o $OUT_DIR/common.o -O0
+	$CPPC -g -fPIC -c config.cpp -Ijson_hpp -std=c++20 -o $OUT_DIR/config.o -O0
+	$CPPC -g -fPIC -c dinput8_hook_adapter.cpp -std=c++20 -o $OUT_DIR/dinput8_hook_adapter.o -O0
+	$CPPC -g -fPIC -c process.cpp -std=c++20 -o $OUT_DIR/process.o -O0
 	$CPPC -g -fPIC -c tdu2_physics_tweaks.cpp -Iminhook_1.3.3/include -std=c++20 -o $OUT_DIR/tdu2_physics_tweaks.o -O0
 	$CPPC -g -shared -o $OUT_DIR/tdu2_physics_tweaks_${arch}.asi $OUT_DIR/logging.o $OUT_DIR/common.o $OUT_DIR/config.o $OUT_DIR/dinput8_hook_adapter.o $OUT_DIR/process.o $OUT_DIR/tdu2_physics_tweaks.o -Lminhook_1.3.3/bin -lntdll -lkernel32 -ldxguid -Wl,-Bstatic -lpthread -l${min_hook_lib} -static-libgcc
 
